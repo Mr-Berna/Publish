e8ed2ff44d1a56d3c158a496a8476b618bb33a86
Eric Berna <eric@sunny.fish>
2025-09-25 22:24:48 -0500

    Point to my clone of Plot.

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .package(
             name: "Plot",
-            url: "https://github.com/johnsundell/plot.git",
-            from: "0.9.0"
+            url: "https://github.com/Mr-Berna/Plot.git",
+            branch: "main"
         ),
         .package(
             name: "Files",
